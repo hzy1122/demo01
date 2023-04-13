c2dd3c5bb043b6f840b361c2c6855ff3c4957a84
hzy <861791991@qq.com>
2023-04-13 11:16:51 +0800

    在hot-fix分支上的第二次提交

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld{
 	public static void main(String[] args){
 		System.out.println("Hello World!");
 		System.out.println("在hot-fix分支上增加的输出语句");
+		System.out.println("在hot-fix分支上增加的第二条输出语句");
 	}
 }
